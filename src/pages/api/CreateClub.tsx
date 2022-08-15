9d3dde88ba96a709831e011d4edf86cbc98b277a
AlexPegg <alexpeggsailing@gmail.com>
2022-08-15 20:10:20 +0000

    Create and Read Clubs

---
+++ src/pages/api/CreateClub.tsx
@@ -0,0 +1,53 @@
+import prisma from '../../components/prisma'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import assert from 'assert';
+
+async function findClub(name: string){
+    var result = await prisma.clubs.findUnique({
+        where: {
+            name: name,
+        },
+    })
+    return result;
+}
+
+async function createClub(name: string){
+    var club = await prisma.clubs.create({
+        data: {
+            name: name
+        },
+    })
+    return club;
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'POST') {
+        // check if we have all data.
+        // The website stops this, but just in case
+        try {
+            assert.notStrictEqual(undefined, req.body.name, 'Name required');
+        } catch (bodyError) {
+            res.status(403).json({error: true, message: "information missing"});
+            return;
+        }
+        
+        var name = req.body.name
+
+        var Existinguser = await findClub(name)
+        if (!Existinguser) {
+            var Club = await createClub(name)
+            if (Club) {
+                res.status(200).json({error: false, Club: Club});
+                return;
+            }
+            else{
+                res.status(500).json({error: true, message: 'Something went wrong crating club'});
+            }
+        } else {
+            // User exists
+            res.status(403).json({error: true, message: 'club already exists'});
+            return;
+        }
+    }
+};
---
+++ src/pages/api/GetClub.tsx
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '../../components/prisma'
+
+async function getClub(name: string){
+    var result = await prisma.clubs.findFirst({
+        where: {
+            name: name
+        }
+    })
+    if (result == null){
+        return
+    }
+    return result;
+}
+
+export default  async(req: NextApiRequest, res: NextApiResponse) => {
+    var name: string = req.body.name
+    console.log(name)
+    if (req.method === 'GET') {
+        var Club = await getClub(name)
+        if(Club){
+            res.status(200).json({error: false, Club: Club});
+        }
+        else{
+            res.status(500).json({error: true, message: 'Could not find club'});
+        }
+    }
+};
