62dc741a1ed000c17825bc35a16a337e15229341
AlexPegg <alexpeggsailing@gmail.com>
2022-10-21 20:54:37 +0100

    fixed join page

--- src/pages/Join.tsx
+++ src/pages/Join.tsx
@@ -4,18 +4,12 @@ import Card from '../components/Card'
 const Join = () => {
     return (
         <Layout>
-            <div className="flex flex-row items-center justify-center h-screen p-4">
-                <div className="flex">
-                    <p>placeholder</p>
-                </div>
-                <div className='flex'>
-                    <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
-                        Join
-                    </h1>
-                    <p>Please Email SRM@alexpegg.uk to enquire about joining</p>
-                </div>
-                
-            </div>
+                <div className="container mx-auto flex flex-col items-center justify-center h-screen p-4">
+                <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
+                    Join
+                </h1>
+                <p>Please Email SRM@alexpegg.uk to enquire about joining</p>
+            </div>    
     </Layout>
   );
 };
