b14bc85f000c2fb487c3bb58b891ca4403d916ec
AlexPegg <alexpeggsailing@gmail.com>
2023-09-10 13:02:20 +0100

    better wording on button

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -441,7 +441,7 @@ const Club = () => {
                         </div>
                         <div className="px-5 py-1 w-3/4">
                             <p onClick={(e) => { confirm("are you sure you want to do this?") ? updateBoatsToLatestRYA() : null; }} className="cursor-pointer text-white bg-blue-600 hover:bg-pink-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0">
-                                Update list to match latest RYA values
+                                Set list to latest RYA values
                             </p>
                         </div>
                         <p className='text-2xl font-bold text-gray-700 p-6'>
