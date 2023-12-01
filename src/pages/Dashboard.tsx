a510daa885343cfc9a756f4efa01ab907e340993
AlexPegg <alexpeggsailing@gmail.com>
2023-12-01 15:21:53 +0000

    removed sign on sheet from race page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -677,11 +677,6 @@ const Club = () => {
                             </div>
                         </div>
                         <div className="p-6 w-3/4">
-                            <p onClick={() => router.push({ pathname: '/SignOn', query: { race: activeRaceData.id } })} className="cursor-pointer text-white bg-blue-600 hover:bg-pink-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0">
-                                Sign-On Sheet
-                            </p>
-                        </div>
-                        <div className="p-6 w-3/4">
                             <p onClick={openRacePanel} className="cursor-pointer text-white bg-blue-600 hover:bg-pink-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0">
                                 Race Panel
                             </p>
