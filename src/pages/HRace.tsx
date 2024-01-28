30fc79ff72ef05b017ca9cb807b6814e67e50718
AlexPegg <alexpeggsailing@gmail.com>
2024-01-28 12:41:31 +0000

    use number of laps in results calculation

--- src/pages/HRace.tsx
+++ src/pages/HRace.tsx
@@ -215,7 +215,7 @@ const RacePage = () => {
     const calculateResults = () => {
         //most nuber of laps.
         console.log(race)
-        const maxLaps = Math.max.apply(null, race.results.map(function (o: ResultsDataType) { return Object.keys(o.lapTimes).length }))
+        const maxLaps = Math.max.apply(null, race.results.map(function (o: ResultsDataType) { return Object.keys(o.lapTimes.times).length }))
         console.log(maxLaps)
         if (!(maxLaps >= 0)) {
             console.log("max laps not more than one")
@@ -227,7 +227,7 @@ const RacePage = () => {
         resultsData.forEach(result => {
             let seconds = result.finishTime - race.startTime
             console.log(seconds)
-            result.CorrectedTime = (seconds * 1000 * (maxLaps / Object.keys(result.lapTimes).length)) / result.boat.py
+            result.CorrectedTime = (seconds * 1000 * (maxLaps / Object.keys(result.lapTimes.times).length)) / result.boat.py
             console.log(result.CorrectedTime)
             if (result.finishTime == -1) {
                 result.CorrectedTime = 99999
