952d12265df9add96a0e96e5199c104ad9e1be11
AlexPegg <alexpeggsailing@gmail.com>
2024-03-29 12:01:21 +0000

    removed button set to RYA values

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -799,11 +799,6 @@ const Club = () => {
                         <div className='p-6'>
                             <BoatTable data={boatData} key={boatData} updateBoat={updateBoat} deleteBoat={deleteBoat} createBoat={createBoat} />
                         </div>
-                        <div className="px-5 py-1 w-3/4">
-                            <p onClick={(e) => { confirm("are you sure you want to do this?") ? updateBoatsToLatestRYA() : null; }} className="cursor-pointer text-white bg-blue-600 hover:bg-pink-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0">
-                                Set list to latest RYA values
-                            </p>
-                        </div>
                         <p className='text-2xl font-bold text-gray-700 p-6'>
                             Pursuit Race Length
                         </p>
