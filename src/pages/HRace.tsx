6b800cf7e39b25b39cb42b7f6b5a807c3a320546
Chris Hargreaves <cjh861@outlook.com>
2024-05-02 22:41:42 +0100

    escape '

--- src/pages/HRace.tsx
+++ src/pages/HRace.tsx
@@ -700,7 +700,7 @@ const RacePage = () => {
                     </div>
                     <div className="flex mb-2 justify-center">
                         <p id="retireNSC" className="w-1/2 cursor-pointer text-white bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0">
-                            Didn't Sail Course (NSC)
+                            Didn&apos;t Sail Course (NSC)
                         </p>
                     </div>
                     <div className="flex mt-8 justify-center">
