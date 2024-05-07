9b567e928e3a57a37de666dc4aee2ba0457ff3ec
AlexPegg <alex@alexpegg.uk>
2024-05-07 17:32:17 +0100

    disable sentry in dev

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -7,6 +7,8 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: "https://5d7e66db551c492fafeccc0a452f9f38@glitchtip.sailviz.com/1",
 
+  enabled: process.env.NODE_ENV === "production",
+
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
