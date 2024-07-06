279160d11212e7af33cf887aa690960e0e0d999e
AlexPegg <alex@alexpegg.uk>
2024-07-06 11:33:02 +0100

    show how many boats have entered

--- src/pages/Race.tsx
+++ src/pages/Race.tsx
@@ -835,7 +835,7 @@ const SignOnPage = () => {
                             return (
                                 <div key={"fleetResults" + index}>
                                     <p className='text-2xl font-bold text-gray-700'>
-                                        {fleet.fleetSettings.name}
+                                        {fleet.fleetSettings.name} - Boats Entered: {fleet.results.length}
                                     </p>
                                     <FleetResultsTable showTime={true} editable={true} data={fleet.results} startTime={fleet.startTime} key={JSON.stringify(race)} deleteResult={deleteResult} updateResult={updateResult} raceId={race.id} showEditModal={(id: string) => { showEditModal(id) }} />
                                     <p onClick={() => createResult(fleet.id)} id="RacePanelButton" className="cursor-pointer text-white bg-blue-600 hover:bg-pink-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 my-5">
