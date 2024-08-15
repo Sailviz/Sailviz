b1b4fe99734bfd21988d311b3d9ce0a162c4fded
AlexPegg <alex@alexpegg.uk>
2024-08-15 18:36:05 +0100

    unlikely fix for error

--- app/api/user/route.tsx
+++ app/api/user/route.tsx
@@ -1,4 +1,4 @@
-import prisma from '../../../components/prisma'
+import prisma from 'components/prisma'
 import { NextRequest, NextResponse } from "next/server";
 
 async function getUser(id: string) {
