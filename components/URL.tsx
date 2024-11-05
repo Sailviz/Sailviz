3e7ae46fa79493d91aaf76ac54324f8cecd7d540
AlexPegg <alex@alexpegg.uk>
2024-11-05 09:26:47 +0000

    change the way the production url is gotten so that can run on dev server for testing

--- components/URL.tsx
+++ components/URL.tsx
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://sailviz.com';
+export const server = dev ? 'http://localhost:3000' : process.env.BASE_URL;
