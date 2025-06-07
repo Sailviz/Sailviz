04606f3a0f0256b62c29939b63763fc54ab8eacd
AlexPegg <alex@alexpegg.uk>
2025-06-07 19:55:49 +0100

    forgot to save the file 🤦

--- src/app/@modal/(...)editResult/[resultId]/page.tsx
+++ src/app/@modal/(...)editResult/[resultId]/page.tsx
@@ -35,7 +35,6 @@ export default function Page(props: PageProps) {
                 updateResult(result)
             }}
             result={result}
-            raceType={''}
             advancedEdit={true}
             onDelete={onDeleteResult}
         />
