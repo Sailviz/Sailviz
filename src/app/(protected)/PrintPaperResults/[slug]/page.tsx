98fbd13641030dd4dd23e139f31371503a6ef7ac
AlexPegg <alex@alexpegg.uk>
2025-06-08 09:32:34 +0100

    fixed race sheet printing

--- src/app/(protected)/PrintPaperResults/[slug]/page.tsx
+++ src/app/(protected)/PrintPaperResults/[slug]/page.tsx
@@ -1,18 +1,19 @@
 'use client'
-import React, { ChangeEvent, MouseEventHandler, useEffect, useRef, useState } from 'react'
+import React, { ChangeEvent, MouseEventHandler, use, useEffect, useRef, useState } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import * as DB from '@/components/apiMethods'
 import { useReactToPrint } from 'react-to-print'
 import HandicapPaperResultsTable from '@/components/tables/HandicapPaperResultsTable'
 import PursuitPaperResultsTable from '@/components/tables/PursuitPaperResultsTable'
 import dayjs from 'dayjs'
+import ReactDOM from 'react-dom'
 
 type PageProps = { params: Promise<{ slug: string }> }
 
-export default async function Page(props: PageProps) {
+export default function Page(props: PageProps) {
     const Router = useRouter()
 
-    const params = await props.params
+    const { slug } = use(props.params)
 
     var [race, setRace] = useState<RaceDataType>({
         id: '',
@@ -66,25 +67,23 @@ export default async function Page(props: PageProps) {
     const contentRef = useRef<HTMLDivElement>(null)
 
     const handlePrint = useReactToPrint({
-        content: () => contentRef.current as any,
+        contentRef,
         onAfterPrint: () => {
             Router.back()
         }
     })
 
     useEffect(() => {
-        let raceId = params.slug
-        console.log(raceId)
         const getRace = async () => {
-            const racedata = await DB.getRaceById(raceId!)
+            const racedata = await DB.getRaceById(slug!)
             setRace(racedata)
-            DB.GetSeriesById(race.seriesId).then(series => series.name)
+            DB.GetSeriesById(racedata.seriesId).then(series => series.name)
         }
 
-        if (raceId != null) {
+        if (slug != undefined) {
             getRace()
         }
-    }, [Router])
+    }, [Router, slug])
 
     useEffect(() => {
         if (race.id != '') {
@@ -94,14 +93,20 @@ export default async function Page(props: PageProps) {
 
     return (
         <div className='h-full overflow-y-auto p-6' ref={contentRef}>
-            <p className='text-2xl'>
-                {race.series.name} - Race {race.number} ({dayjs(race.Time).format('DD/MM/YYYY HH:mm')})
-            </p>
-            {race.Type == 'Handicap' ? (
-                <HandicapPaperResultsTable results={race.fleets.flatMap(fleet => fleet.results)} key={JSON.stringify(race)} />
-            ) : (
-                <PursuitPaperResultsTable results={race.fleets.flatMap(fleet => fleet.results)} key={JSON.stringify(race)} />
-            )}
+            {race!.fleets.map((fleet, index) => {
+                return (
+                    <div key={'fleetResults' + index}>
+                        <p className='text-2xl'>
+                            {race.series.name} - Race {race.number} ({dayjs(race.Time).format('DD/MM/YYYY HH:mm')}) - {fleet.fleetSettings.name}
+                        </p>
+                        {race.Type == 'Handicap' ? (
+                            <HandicapPaperResultsTable results={race.fleets.flatMap(fleet => fleet.results)} key={JSON.stringify(race)} />
+                        ) : (
+                            <PursuitPaperResultsTable results={race.fleets.flatMap(fleet => fleet.results)} key={JSON.stringify(race)} />
+                        )}
+                    </div>
+                )
+            })}
         </div>
     )
 }
