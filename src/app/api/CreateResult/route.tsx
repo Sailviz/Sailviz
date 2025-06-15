b89a8bc919d355225fb9cb71b9f133cd6dfa41bb
AlexPegg <alex@alexpegg.uk>
2025-06-15 10:24:11 +0100

    fix signon error

--- src/app/api/CreateResult/route.tsx
+++ src/app/api/CreateResult/route.tsx
@@ -34,7 +34,8 @@ async function createEntry(fleetId: string) {
             resultCode: ''
         },
         include: {
-            laps: true
+            laps: true,
+            fleet: true
         }
     })
     return res
