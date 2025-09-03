e2e825dd76139ff176655e2b44c0dd2321c4a7e3
AlexPegg <alex@alexpegg.uk>
2025-09-03 21:31:58 +0100

    #251 Race sheet includes results if they are available

--- src/components/tables/HandicapPaperResultsTable.tsx
+++ src/components/tables/HandicapPaperResultsTable.tsx
@@ -1,87 +1,130 @@
 import React, { forwardRef, useState } from 'react'
-import { createColumnHelper, flexRender, getCoreRowModel, getSortedRowModel, useReactTable, SortingState } from '@tanstack/react-table'
+import { createColumnHelper, flexRender, getCoreRowModel, getSortedRowModel, useReactTable, SortingState, ColumnDef } from '@tanstack/react-table'
+import { data } from 'cypress/types/jquery'
+import { start } from 'repl'
 
-const Text = ({ ...props }) => {
-    const value = props.getValue()
-
-    return <div className=' text-center'>{value}</div>
+const Text = ({ text }: { text: string }) => {
+    return <div className=' text-center'>{text}</div>
 }
 
-const Empty = ({ ...props }) => {
+const Empty = () => {
     return <div />
+}
+
+const Time = ({ time, startTime }: { time: number; startTime: number }) => {
+    if (time == 0 || startTime == 0) {
+        return <p className='p-2 m-2 text-center w-full'></p>
+    }
+    const [value, setValue] = React.useState(new Date((time - startTime) * 1000).toISOString().substring(11, 19))
+    if (time == -1) {
+        return <p className='p-2 m-2 text-center w-full'>Retired</p>
+    } else if (time == 0) {
+        return <p className='p-2 m-2 text-center w-full'>-</p>
+    } else {
+        return <p className='p-2 m-2 text-center w-full'> {value}</p>
+    }
+}
+
+const CorrectedTime = ({ ...props }) => {
+    let value = Math.round(props.getValue())
+    let result = props.result
+    let valueString = ''
+    if (result.resultCode != '') {
+        valueString = result.resultCode
+    } else {
+        valueString = value.toString()
+    }
+    if (valueString == '0') {
+        valueString = ''
+    }
+
+    return <div className=' text-center'>{valueString}</div>
 }
 
 const columnHelper = createColumnHelper<ResultDataType>()
 
-const HandicapPaperResultsTable = forwardRef((props: { results: ResultDataType[] }, ref: any) => {
-    let [results, setResults] = useState<ResultDataType[]>(props.results)
+const HandicapPaperResultsTable = forwardRef((props: { fleet: FleetDataType }, ref: any) => {
+    let [results, setResults] = useState<ResultDataType[]>(props.fleet.results)
+    const fleet = props.fleet
 
-    //create 3 empty lines on sheet
-    results.push({} as ResultDataType)
-    results.push({} as ResultDataType)
-    results.push({} as ResultDataType)
+    let allFinished = true
+    results.forEach(data => {
+        if (data.finishTime == 0 && data.resultCode == '') {
+            allFinished = false
+        }
+    })
+
+    if (!allFinished) {
+        //create 3 empty lines on an empty sheet only
+        results.push({} as ResultDataType)
+        results.push({} as ResultDataType)
+        results.push({} as ResultDataType)
+    }
 
     //sets sorting to position by default
     const [sorting, setSorting] = useState<SortingState>([
         {
-            id: 'PY',
+            id: allFinished ? 'HandicapPosition' : 'PY',
             desc: false
         }
     ])
 
-    let columns = [
+    let columns: ColumnDef<ResultDataType, any>[] = [
         columnHelper.accessor('Helm', {
             header: 'Helm',
-            cell: props => <Text {...props} />,
+            cell: props => <Text text={props.getValue()} />,
             enableSorting: false
         }),
         columnHelper.accessor('Crew', {
             header: 'Crew',
-            cell: props => <Text {...props} />,
+            cell: props => <Text text={props.getValue()} />,
             enableSorting: false
         }),
         columnHelper.accessor(data => data.boat?.name, {
             header: 'Class',
             id: 'Class',
-            cell: props => <Text {...props} />,
+            cell: props => <Text text={props.getValue()} />,
             enableSorting: false
         }),
         columnHelper.accessor(data => data.SailNumber, {
             header: 'Sail Number',
-            cell: props => <Text {...props} />,
+            cell: props => <Text text={props.getValue()} />,
             enableSorting: false
         })
     ]
 
-    // add column for each lap
-    for (let i = 0; i < 6; i++) {
-        const newColumn = columnHelper.display({
-            header: (i + 1).toString(),
-            size: 40,
-            cell: props => <Empty {...props} />,
-            enableSorting: false
-        })
-        columns.push(newColumn)
+    if (!allFinished) {
+        // add column for each lap
+        for (let i = 0; i < 6; i++) {
+            const newColumn = columnHelper.display({
+                header: (i + 1).toString(),
+                size: 40,
+                cell: props => <Empty />,
+                enableSorting: false
+            })
+            columns.push(newColumn)
+        }
     }
-
-    const ElapsedTime = columnHelper.display({
+    const ElapsedTime = columnHelper.accessor(data => data.finishTime, {
         header: 'Elapsed Time',
-        cell: props => <Empty {...props} />,
+        cell: props => <Time time={props.getValue()} startTime={fleet.startTime} />,
         enableSorting: false
     })
     columns.push(ElapsedTime)
 
-    const Seconds = columnHelper.display({
-        header: 'Seconds',
-        cell: props => <Empty {...props} />,
-        enableSorting: false
-    })
-    columns.push(Seconds)
+    if (!allFinished) {
+        const Seconds = columnHelper.display({
+            header: 'Seconds',
+            cell: props => <Empty />,
+            enableSorting: false
+        })
+        columns.push(Seconds)
+    }
 
-    const PY = columnHelper.accessor(data => data.boat?.py.toString() || '-', {
+    const PY = columnHelper.accessor(data => data.boat?.py.toString() || '', {
         header: 'PY',
         id: 'PY',
-        cell: props => <Text {...props} />,
+        cell: props => <Text text={props.getValue()} />,
         enableSorting: true,
         sortingFn: (rowA, rowB, columnId) => {
             if (rowA.original.boat?.py == undefined) {
@@ -95,17 +138,17 @@ const HandicapPaperResultsTable = forwardRef((props: { results: ResultDataType[]
     })
     columns.push(PY)
 
-    const Correctedtime = columnHelper.display({
+    const Correctedtime = columnHelper.accessor('CorrectedTime', {
         header: 'Corrected Time',
-        cell: props => <Empty {...props} />,
+        cell: props => <CorrectedTime {...props} result={results.find(result => result.id == props.row.original.id)} />,
         enableSorting: false
     })
     columns.push(Correctedtime)
 
-    const Position = columnHelper.display({
+    const Position = columnHelper.accessor('HandicapPosition', {
         header: 'Position',
-        cell: props => <Empty {...props} />,
-        enableSorting: false
+        cell: props => <Text text={props.getValue() == 0 ? '' : props.getValue()?.toString() || ''} />,
+        enableSorting: true
     })
     columns.push(Position)
 
@@ -120,7 +163,7 @@ const HandicapPaperResultsTable = forwardRef((props: { results: ResultDataType[]
         getSortedRowModel: getSortedRowModel()
     })
     return (
-        <div key={JSON.stringify(props.results)} className='block max-w-full' ref={ref}>
+        <div className='block max-w-full' ref={ref}>
             <table className='w-full border-spacing-0'>
                 <thead>
                     {table.getHeaderGroups().map(headerGroup => (
