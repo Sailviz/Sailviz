198705dc6f4649fb1bc2e5afafa0d192f56bfcad
AlexPegg <alex@alexpegg.uk>
2025-09-09 21:13:26 +0100

    functionality added to create event on homepage

--- src/components/layout/dashboard/CreateEventModal.tsx
+++ src/components/layout/dashboard/CreateEventModal.tsx
@@ -1,11 +1,136 @@
 'use client'
 import { Button } from '@/components/ui/button'
-import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTrigger } from '@/components/ui/dialog'
+import { Dialog, DialogContent, DialogFooter, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import * as DB from '@/components/apiMethods'
-import { mutate } from 'swr'
 import { useSession } from '@/lib/auth-client'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import dayjs from 'dayjs'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+import { createColumnHelper, flexRender, getCoreRowModel, getFilteredRowModel, useReactTable } from '@tanstack/react-table'
+import { getFiveStartSequence, getThreeStartSequence } from '@/components/helpers/startSequence'
+
+type Race = {
+    number: number
+    time: string
+    type: string
+}
+
+const raceOptions = [
+    { value: 'Pursuit', label: 'Pursuit' },
+    { value: 'Handicap', label: 'Handicap' }
+]
+
+const startSequenceOptions = [
+    { value: 'Five', label: '5 4 1 0' },
+    { value: 'Three', label: '3 2 1 0' }
+]
+
+const Text = ({ value }: { value: string }) => {
+    return <div>{value}</div>
+}
+
+const Time = ({ initialValue, race, updateTime }: { initialValue: any; race: Race; updateTime: (number: number, time: string) => void }) => {
+    const [value, setValue] = useState(initialValue)
+
+    const onBlur = () => {
+        console.log(value)
+        var time = value.replace('T', ' ')
+        var day = dayjs(time)
+        if (day.isValid()) {
+            updateTime(race.number, time)
+        }
+    }
+
+    useEffect(() => {
+        setValue(initialValue)
+    }, [initialValue])
+
+    return (
+        <>
+            <Input
+                type='datetime-local'
+                id='Time'
+                className='w-full'
+                value={dayjs(value).format('YYYY-MM-DDTHH:mm')}
+                onChange={e => setValue(e.target.value)}
+                onBlur={onBlur}
+            />
+        </>
+    )
+}
+
+const Type = ({ initialValue, race, updateType }: { initialValue: any; race: Race; updateType: (number: number, type: string) => void }) => {
+    const [value, setValue] = useState(initialValue)
+
+    const onBlur = (type: string) => {
+        race.type = type
+        updateType(race.number, type)
+    }
+
+    useEffect(() => {
+        setValue(initialValue)
+    }, [initialValue])
+
+    return (
+        <>
+            <Select
+                value={value}
+                onValueChange={(value: string) => {
+                    setValue(value)
+                    onBlur(value)
+                }}
+            >
+                <SelectTrigger className='w-full'>
+                    <SelectValue placeholder='Select Type' />
+                </SelectTrigger>
+                <SelectContent>
+                    {raceOptions.map(option => (
+                        <SelectItem key={option.value} value={option.value}>
+                            {option.label}
+                        </SelectItem>
+                    ))}
+                </SelectContent>
+            </Select>
+        </>
+    )
+}
+
+const StartSequence = ({
+    initialValue,
+    race,
+    updateStartSequence
+}: {
+    initialValue: any
+    race: Race
+    updateStartSequence: (number: number, startSequence: string) => void
+}) => {
+    const [value, setValue] = useState(initialValue)
+
+    const onBlur = (startSequence: string) => {
+        updateStartSequence(race.number, startSequence)
+    }
+
+    useEffect(() => {
+        setValue(initialValue)
+    }, [initialValue])
+
+    return <></>
+}
+const Action = ({ number, removeRace }: { number: number; removeRace: any }) => {
+    const onDeleteClick = async () => {
+        removeRace(number)
+    }
+    return (
+        <div className='relative flex items-center gap-2'>
+            <Button onClick={onDeleteClick} variant={'outline'}>
+                Remove
+            </Button>
+        </div>
+    )
+}
+const columnHelper = createColumnHelper<Race>()
 
 export default function CreateEventDialog() {
     const {
@@ -15,22 +140,118 @@ export default function CreateEventDialog() {
         refetch //refetch the session
     } = useSession()
 
-    const [name, setName] = useState('')
-    const [numberOfRaces, setNumberOfRaces] = useState(0)
+    const [races, setRaces] = useState<Race[]>([{ number: 1, time: dayjs().format('YYYY-MM-DD HH:mm'), type: 'Handicap' }])
+    const [name, setName] = useState('New Series')
+    const [startSequence, setStartSequence] = useState('Five')
 
     const [open, setOpen] = useState(false)
 
-    const createEvent = async (name: string, numberOfRaces: number) => {
-        if (!session?.club) {
-            throw new Error('No club found in session')
+    const addRace = () => {
+        //these aren't actually the numbers that the race will get as they are assigned in the api.
+        setRaces([...races, { number: races.length + 1, time: dayjs().format('YYYY-MM-DD HH:mm'), type: 'Handicap' }])
+    }
+
+    const removeRace = (number: number) => {
+        setRaces((prevRaces: Race[]) => prevRaces.filter((r: Race) => r.number !== number))
+    }
+
+    const updateTime = (number: number, time: string) => {
+        setRaces(prevRaces =>
+            prevRaces.map(r => {
+                if (r.number === number) {
+                    return { ...r, time: time }
+                }
+                return r
+            })
+        )
+    }
+
+    const updateType = (number: number, type: string) => {
+        setRaces(prevRaces =>
+            prevRaces.map(r => {
+                if (r.number === number) {
+                    return { ...r, type: type }
+                }
+                return r
+            })
+        )
+    }
+
+    const updateStartSequence = (number: number, startSequence: string) => {
+        setRaces(prevRaces =>
+            prevRaces.map(r => {
+                if (r.number === number) {
+                    return { ...r, startSequence: startSequence }
+                }
+                return r
+            })
+        )
+    }
+
+    const createEvent = async (events: Race[]) => {
+        console.log('Creating event', events)
+        //create series
+        const res = await DB.createSeries(session?.user.clubId!, name) // this adds a single fleet to the series by default
+        if (!res) {
+            console.error('Failed to create series')
+            return
         }
-        const series = await DB.createSeries(session.club.id, name)
-        for (let i = 0; i < numberOfRaces; i++) {
-            await DB.createRace(session.club.id, series.id)
+        console.log('Created series', res)
+        const series = await DB.GetSeriesById(res.id)
+        if (!series) {
+            console.error('Failed to fetch series after creation')
+            return
         }
-        mutate('/api/GetTodaysRaceByClubId')
+        console.log('Fetched series', series)
+        // set start sequence for series
+        if (startSequence === 'Five') {
+            await DB.updateStartSequenceById(series.id, getFiveStartSequence(series.fleetSettings[0]?.id || ''))
+        } else if (startSequence === 'Three') {
+            await DB.updateStartSequenceById(series.id, getThreeStartSequence(series.fleetSettings[0]?.id || ''))
+        } else {
+            console.error('Invalid start sequence')
+            return
+        }
+        //create each race
+        for (const event of events) {
+            const dbrace = await DB.createRace(session?.user.clubId!, series.id)
+            //update race with details
+            dbrace.Type = event.type
+            dbrace.Time = event.time
+            await DB.updateRaceById(dbrace)
+        }
+
         setOpen(false)
     }
+
+    var table = useReactTable({
+        data: races,
+        columns: [
+            columnHelper.accessor('number', {
+                header: 'Number',
+                cell: props => <Text value={props.getValue()} />,
+                enableColumnFilter: true
+            }),
+            columnHelper.accessor('time', {
+                header: 'Time',
+                cell: props => <Time initialValue={props.getValue()} race={props.row.original} updateTime={updateTime} />,
+                enableColumnFilter: false
+            }),
+            columnHelper.accessor('type', {
+                id: 'py',
+                header: 'Type',
+                cell: props => <Type initialValue={props.getValue()} race={props.row.original} updateType={updateType} />,
+                enableColumnFilter: false
+            }),
+            columnHelper.accessor('number', {
+                id: 'action',
+                header: 'Actions',
+                cell: props => <Action number={props.getValue()} removeRace={removeRace} />
+            })
+        ],
+        getCoreRowModel: getCoreRowModel(),
+        getFilteredRowModel: getFilteredRowModel()
+    })
 
     return (
         <Dialog
@@ -42,27 +263,58 @@ export default function CreateEventDialog() {
             <DialogTrigger asChild aria-label='new event'>
                 <Button>Create New Event</Button>
             </DialogTrigger>
-            <DialogContent>
-                <DialogHeader className='flex flex-col gap-1'>Create New Event</DialogHeader>
-                <div className='flex w-full'>
-                    <div className='flex flex-col px-6 w-full'>
-                        <p className='text-2xl font-bold'>Name</p>
+            <DialogContent className='max-w-8/12'>
+                <DialogTitle className='flex flex-col gap-1'>Create New Event</DialogTitle>
+                <div className='flex flex-row w-full'>
+                    <p className='text-2xl font-bold'>Name</p>
 
-                        <Input id='name' type='text' value={name} onChange={e => setName(e.target.value)} autoComplete='off' />
-                    </div>
-                    <div className='flex flex-col px-6 w-full'>
-                        <p className='text-2xl font-bold whitespace-nowrap'>Number Of Races</p>
-                        <Input
-                            id='numberOfRaces'
-                            type='number'
-                            value={numberOfRaces.toString()}
-                            onChange={e => setNumberOfRaces(parseInt(e.target.value))}
-                            autoComplete='off'
-                        />
-                    </div>
+                    <Input type='text' className='mx-4 w-1/4' value={name} onChange={e => setName(e.target.value)} />
+                    <p className='text-2xl font-bold  text-nowrap'>Start Sequence</p>
+
+                    <Select
+                        value={startSequence}
+                        onValueChange={(value: string) => {
+                            setStartSequence(value)
+                        }}
+                    >
+                        <SelectTrigger className='w-1/4 mx-4'>
+                            <SelectValue placeholder='Select Type' />
+                        </SelectTrigger>
+                        <SelectContent>
+                            {startSequenceOptions.map(option => (
+                                <SelectItem key={option.value} value={option.value}>
+                                    {option.label}
+                                </SelectItem>
+                            ))}
+                        </SelectContent>
+                    </Select>
                 </div>
+
+                <Table>
+                    <TableHeader>
+                        {table.getHeaderGroups().map(headerGroup => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map(header => {
+                                    return (
+                                        <TableHead key={header.id}>{header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}</TableHead>
+                                    )
+                                })}
+                            </TableRow>
+                        ))}
+                    </TableHeader>
+                    <TableBody>
+                        {table.getRowModel().rows.map(row => (
+                            <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'} className='cursor-pointer'>
+                                {row.getVisibleCells().map(cell => (
+                                    <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+                                ))}
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+                <Button onClick={() => addRace()}>Add Race</Button>
                 <DialogFooter>
-                    <Button color='primary' onClick={() => createEvent(name, numberOfRaces)}>
+                    <Button color='primary' onClick={() => createEvent(races)}>
                         Create
                     </Button>
                 </DialogFooter>
