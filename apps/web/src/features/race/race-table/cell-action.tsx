5c9e5c4e329bac64351f471b9bface1dedc21202
AlexPegg <alex@alexpegg.uk>
2026-05-21 22:56:41 +0100

    fix action path for race

--- apps/web/src/features/race/race-table/cell-action.tsx
+++ apps/web/src/features/race/race-table/cell-action.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@components/ui/button'
-import { useRouter } from '@tanstack/react-router'
+import { useRouter, useRouterState } from '@tanstack/react-router'
 import * as Types from '@sailviz/types'
 
 interface CellActionProps {
@@ -8,10 +8,13 @@ interface CellActionProps {
 
 export const CellAction: React.FC<CellActionProps> = ({ data }) => {
     const router = useRouter()
+    const routerState = useRouterState()
+
+    const redirectPath = routerState.location.pathname.toLowerCase() == '/dashboard/race' ? `${data.id}` : `race/${data.id}`
 
     return (
         <>
-            <Button className=' cursor-pointer' onClick={() => router.navigate({ to: `race/${data.id}` })}>
+            <Button className=' cursor-pointer' onClick={() => router.navigate({ to: redirectPath })}>
                 Open
             </Button>
         </>
