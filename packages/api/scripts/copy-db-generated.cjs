7764007e3b6e3d3572f956c396f2223237ae6243
AlexPegg <alex@alexpegg.uk>
2026-05-24 11:13:18 +0100

    fixed types export I think

--- packages/api/scripts/copy-db-generated.cjs
+++ packages/api/scripts/copy-db-generated.cjs
@@ -2,18 +2,10 @@
 const fs = require("fs");
 const path = require("path");
 
-const src = path.join(__dirname, "..", "..", "db", "dist", "generated");
-const dest1 = path.join(
-  __dirname,
-  "..",
-  "dist",
-  "packages",
-  "db",
-  "src",
-  "generated",
-);
+const src = path.join(__dirname, "..", "..", "db", "dist");
+const dest1 = path.join(__dirname, "..", "dist", "packages", "db", "src");
 
-const dest2 = path.join(__dirname, "..", "dist", "db", "src", "generated");
+const dest2 = path.join(__dirname, "..", "dist", "db", "src");
 
 function copyRecursiveSync(src, dest) {
   if (!fs.existsSync(src)) return;
@@ -32,3 +24,8 @@ copyRecursiveSync(src, dest1);
 console.log(`Copied generated prisma client from ${src} to ${dest1}`);
 copyRecursiveSync(src, dest2);
 console.log(`Copied generated prisma client from ${src} to ${dest2}`);
+
+// copy package.json
+fs.copyFileSync("package.json", "dist/package.json");
+
+copyRecursiveSync("dist/types", "dist/packages/types");
