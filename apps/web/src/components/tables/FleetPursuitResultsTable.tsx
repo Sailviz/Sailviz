770a9e6d50a4f3ce97e36ae122fa14acc54a3c0a
AlexPegg <alex@alexpegg.uk>
2026-08-29 11:10:57 +0100

    add map column to pursuit race table

--- apps/web/src/components/tables/FleetPursuitResultsTable.tsx
+++ apps/web/src/components/tables/FleetPursuitResultsTable.tsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
+import { useState, type ChangeEvent } from 'react'
 import { createColumnHelper, flexRender, getCoreRowModel, getSortedRowModel, useReactTable, type SortingState } from '@tanstack/react-table'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@components/ui/table'
 import { Button } from '@components/ui/button'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { orpcClient } from '@lib/orpc'
 import type { BoatType, ResultType } from '@sailviz/types'
 import EditResultModal from '@components/layout/dashboard/EditResultModal'
 import ViewResultDialog from '@components/layout/dashboard/viewResultModal'
+import { MapPin, Upload } from 'lucide-react'
+import type { Session } from '@lib/session'
+import { useLoaderData } from '@tanstack/react-router'
+import { Input } from '@components/ui/input'
+import { useUploadGPXFile } from '@features/activities/upload'
+import { queryClient } from '@lib/queryClient'
+import MapDialog from '@components/layout/dashboard/MapModal'
 
 const Text = ({ value }: { value: string }) => {
     return <div>{value}</div>
@@ -19,10 +26,15 @@ const columnHelper = createColumnHelper<ResultType>()
 
 const FleetPursuitResultsTable = ({ fleetId, editable, advancedEdit }: { fleetId: string; editable: boolean; advancedEdit: boolean }) => {
     const { data: fleet } = useQuery(orpcClient.fleet.find.queryOptions({ input: { fleetId } }))
-
+    const { data: race } = useQuery(orpcClient.race.find.queryOptions({ input: { raceId: fleet?.raceId || '' }, enabled: fleet !== undefined }))
+    const session: Session = useLoaderData({ from: `__root__` })
     const [editModalOpen, setEditModalOpen] = useState(false)
     const [viewModalOpen, setViewModalOpen] = useState(false)
     const [modalData, setModalData] = useState<ResultType | undefined>(undefined)
+
+    const { uploadGPXFile } = useUploadGPXFile()
+    const linkActivityToResult = useMutation(orpcClient.activity.linkToResult.mutationOptions())
+    const [mapModalOpen, setMapModalOpen] = useState(false)
 
     let data = fleet?.results
     if (data == undefined) {
@@ -106,6 +118,58 @@ const FleetPursuitResultsTable = ({ fleetId, editable, advancedEdit }: { fleetId
         columns.push(viewColumn)
     }
 
+    const entryFileUploadHandler = async (e: ChangeEvent<HTMLInputElement>, resultId: string) => {
+        const inputEl = e.target
+        const file = inputEl.files?.[0]
+        if (!file) {
+            return
+        }
+
+        const activity = await uploadGPXFile(file, resultId)
+        if (!activity) {
+            console.error('Failed to upload activity')
+            return
+        }
+        await linkActivityToResult.mutateAsync({ activityId: activity.id, resultId })
+        // Reset the input so selecting the same file again will fire onChange
+
+        await queryClient.invalidateQueries({ queryKey: orpcClient.fleet.find.queryKey({ input: { fleetId } }) })
+    }
+
+    const trackableColumn = columnHelper.accessor('id', {
+        id: 'Trackable',
+        header: 'Map',
+        cell: props => {
+            if (props.row.original.trackableParticipantId || props.row.original.activityId) {
+                return (
+                    <Button
+                        onClick={() => {
+                            setMapModalOpen(true)
+                            setModalData(props.row.original)
+                        }}
+                    >
+                        <MapPin />
+                    </Button>
+                )
+            } else if (!('error' in session)) {
+                if (props.row.original.userId == session.user.id && race?.trackableEventId != null) {
+                    return (
+                        <div className='flex flex-row'>
+                            <Button className='mx-1 w-1/4' onClick={() => document.getElementById('entryFileUpload')!.click()}>
+                                <Upload />
+                            </Button>
+                            <Input id='entryFileUpload' type='file' accept='.gpx' onChange={e => entryFileUploadHandler(e, props.row.original.id)} className='hidden' />
+                        </div>
+                    )
+                }
+            } else {
+                return <div className='text-center'>-</div>
+            }
+        }
+    })
+
+    columns.push(trackableColumn)
+
     let table = useReactTable({
         data,
         columns: columns,
@@ -118,6 +182,7 @@ const FleetPursuitResultsTable = ({ fleetId, editable, advancedEdit }: { fleetId
     })
     return (
         <div className='w-full'>
+            <MapDialog open={mapModalOpen} result={modalData} onClose={() => setMapModalOpen(false)} />
             <EditResultModal open={editModalOpen} result={modalData} advancedEdit={advancedEdit} onClose={() => setEditModalOpen(false)} />
             <ViewResultDialog open={viewModalOpen} result={modalData} onClose={() => setViewModalOpen(false)} />
             <div className='flex items-center py-4'>
